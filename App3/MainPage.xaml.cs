0944127545b4a2365dcceb1109eafd8815283547
Brian Macomber <brian@agspectrum.com>
2021-02-11 13:13:46 -0600

    Add project files.

---
+++ App3/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+
+namespace App3
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_OnClicked(object sender, EventArgs e)
+		{
+			App.Current.MainPage.Navigation.ShowPopup(new TestPopup());
+		}
+	}
+}
